052a9bbb4a0782be11f4d3a3ffc7be03498b926f
Mariusz Krukowski <Mariusz.Krukowski@aol.com>
2023-05-01 19:22:24 +0200

    Safe view

--- App.js
+++ App.js
@@ -1,11 +1,11 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, SafeAreaView} from 'react-native';
 
 export default function App() {
   return (
-    <View>
-
-    </View>
+    <SafeAreaView>
+      <Text>hello world!!</Text>
+    </SafeAreaView>
   );
 }
 
