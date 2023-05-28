9385ec05735a51323a98855729e1866e073b5ba5
Mariusz Krukowski <Mariusz.Krukowski@aol.com>
2023-05-28 18:36:27 +0200

    yarn update

--- App.js
+++ App.js
@@ -1,14 +1,35 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, SafeAreaView} from 'react-native';
+import { StyleSheet, Text, View, SafeAreaView } from 'react-native';
 
 export default function App() {
   return (
-    <SafeAreaView>
-      <Text>hello world!!</Text>
-    </SafeAreaView>
+    <>
+      <SafeAreaView style={styles.main}> 
+        <View style={styles.searchBox}>
+          <Text>Search</Text>
+        </View>
+        <View style={styles.listBox}>
+          <Text>list</Text>
+        </View>
+      </SafeAreaView>
+      <StatusBar style='auto' />
+    </>
   );
 }
 
 const styles = StyleSheet.create({
+  main:{
+    flex:1
+  },
+  searchBox: {
+    backgroundColor: 'green',
+    padding: 25,
+    flex:0
+  },
+  listBox: {
+    backgroundColor: 'blue',
+    padding: 25,
+    flex:1
 
+  },
 });
