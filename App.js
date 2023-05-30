c600b5b1ecb0c0d95850e0faf9bb08a7d69498de
Mariusz Krukowski <Mariusz.Krukowski@aol.com>
2023-05-30 17:23:06 +0200

    cleaning main app

--- App.js
+++ App.js
@@ -1,39 +1,13 @@
-import { StatusBar as ExpoStatusBar } from "expo-status-bar";
 import React from "react";
-import { Searchbar } from "react-native-paper";
-import { StatusBar, StyleSheet, Text, View, SafeAreaView } from "react-native";
 
+import { StatusBar as ExpoStatusBar } from "expo-status-bar";
+import { RestaurantsScreen } from "./src/features/restaurants/screens/restaurants.screen";
 
 export default function App() {
   return (
     <>
-      <SafeAreaView style={styles.main}>
-        <View style={styles.searchBox}>
-          <Searchbar />
-        </View>
-
-        <View style={styles.listBox}>
-          <Text>list</Text>
-        </View>
-      </SafeAreaView>
-
+      <RestaurantsScreen />
       <ExpoStatusBar style="auto" />
     </>
   );
 }
-
-const styles = StyleSheet.create({
-  main: {
-    flex: 1,
-    marginTop: StatusBar.currentHeight,
-  },
-  searchBox: {
-    padding: 25,
-    flex: 0,
-  },
-  listBox: {
-    backgroundColor: "blue",
-    padding: 25,
-    flex: 1,
-  },
-});
